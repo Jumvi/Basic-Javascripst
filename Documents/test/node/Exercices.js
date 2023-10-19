a6b3360b6e6d27420fd46782b28204d1931538df
Jumvi <judahmvi@gmail.com>
2023-10-19 12:55:40 +0100

    add the fuction's return a mean of two numbers

--- Documents/test/node/Exercices.js
+++ Documents/test/node/Exercices.js
@@ -15,15 +15,32 @@
 
 // exo2 Return one digit from un number
 
-function OnesDigit(nombre){
-    const number = nombre.toString()
-    for(let i = 1; i< number.length -1;i++){
-        let myIndex =Math.floor(Math.random()* ((number.length -1) - 1 +1) + 1) ;
-        console.log(number.at(myIndex));
-    break 
-        i++   
-    }
+// function OnesDigit(nombre){
+//     const number = nombre.toString()
+//     for(let i = 1; i< number.length -1;i++){
+//         let myIndex =Math.floor(Math.random()* ((number.length -1) - 1 +1) + 1) ;
+//         console.log(number.at(myIndex));
+//     break 
+//         i++   
+//     }
+// }
+
+// OnesDigit(123451);
+
+// define  function's take 2 numbers and return their mean
+
+function meanAvg(a,b){
+    const number = [a,b];
+   const sum = number.reduce((a,b)=>{
+        return a+b;
+    })
+
+    const avg = sum / number.length ;
+    console.log (avg);
 }
 
-OnesDigit(123451);
+// Execute function
+
+meanAvg(8,2);
+
 
